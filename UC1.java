11f1857044d58f276c69ff54087df1449aa1cc2c
Rimil Bhattacharya <rbhstorm8@gmail.com>
2026-03-11 16:05:24 +0530

    Update and rename UC1.txt to UC1.java

---
+++ UC1.java
@@ -0,0 +1,14 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        // UC1: Application Entry & Welcome Message
+        System.out.println("===================================");
+        System.out.println("Welcome to the Palindrome Checker!");
+        System.out.println("Application Version: 1.0");
+        System.out.println("Author: RIMIL08X");
+        System.out.println("===================================\n");
+
+        // Continue to next use case (placeholder)
+        System.out.println("Ready to check palindromes!");
+    }
+}
